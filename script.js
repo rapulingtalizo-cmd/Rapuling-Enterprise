cd17f11afa30b4ca8639bdc6d97a3ee4f981ead4
rapulingtalizo-cmd <202638281@spu.ac.za>
2026-05-06 23:38:00 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-let username;
-
-document.getElementById("mySubmit").onclick = function(){
-    username = document.getElementById("myText").value;
-    console.log(username);
-    document.getElementById("p").textContent = "Hello " + username + "!" ;
-}
